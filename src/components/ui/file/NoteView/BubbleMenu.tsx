3a7e87b122c515568e23d43632ece6d647e306d2
Luis <luisoos@users.noreply.github.com>
2024-12-02 21:32:32 +0100

    ✨ feat: finish bubble menu and highlight dropdown

--- src/components/ui/file/NoteView/BubbleMenu.tsx
+++ src/components/ui/file/NoteView/BubbleMenu.tsx
@@ -1,19 +1,30 @@
 import { Editor } from '@tiptap/core';
 import { BubbleMenu } from '@tiptap/react';
 import {
+    BetweenHorizonalEnd,
+    BetweenHorizonalStart,
+    BetweenVerticalEnd,
+    BetweenVerticalStart,
     Bold,
+    Grid2x2X,
+    Heading,
     Italic,
     LinkIcon,
+    SquareSplitHorizontal,
     Strikethrough,
     Subscript,
     Superscript,
     Table,
+    TableColumnsSplit,
+    Trash,
     Underline,
+    WandSparkles,
 } from 'lucide-react';
 import { useCallback, useState } from 'react';
 import getButtonClasses from '~/components/ui/file/NoteView/getButtonClasses';
 import { cn } from '~/hooks/utils';
 import { HighlightDropdown } from './HighlightDropdown';
+import { motion } from 'framer-motion';
 
 const tableBoilerplate = `
   <table style="width:100%" class="not-prose">
@@ -40,8 +51,11 @@ const tableBoilerplate = `
   </table>
 `;
 
+const tableClasses = 'flex h-6 border-none w-full text-left text-sm';
+
 export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
     const [isHovered, setIsHovered] = useState(false);
+    const [showTableMenu, setShowTableMenu] = useState(true);
 
     const setLink = useCallback(() => {
         if (!editor) {
@@ -74,12 +88,12 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
     return (
         <BubbleMenu
             className={cn(
-                'h-7 divide-x rounded border shadow-inner bg-white',
-                editor.isActive('table') ? 'w-[19rem] px-auto' : '',
+                'h-7 pt-auto z-50 divide-x rounded border shadow-inner bg-white',
             )}
             tippyOptions={{ duration: 100 }}
             editor={editor}>
             <button
+                title='Bold'
                 onClick={() => editor.chain().focus().toggleBold().run()}
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
@@ -95,16 +109,19 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
                 />
             </button>
             <button
+                title='Italic'
                 onClick={() => editor.chain().focus().toggleItalic().run()}
                 className={getButtonClasses(editor, 'italic')}>
                 <Italic size={16} />
             </button>
             <button
+                title='Strikethrough'
                 onClick={() => editor.chain().focus().toggleStrike().run()}
                 className={getButtonClasses(editor, 'strike')}>
                 <Strikethrough size={16} />
             </button>
             <button
+                title='Underline'
                 onClick={() =>
                     editor.isActive('underline')
                         ? editor.chain().focus().setUnderline().run()
@@ -113,12 +130,12 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
                 className={cn(
                     getButtonClasses(editor),
                     editor.isActive('underline') ? 'opacity-80' : '',
-                    'my-0.5',
                 )}>
                 <Underline size={16} />
             </button>
-            <HighlightDropdown editor={editor} />
+            <HighlightDropdown editor={editor} requirements={showTableMenu} />
             <button
+                title='Subscript'
                 onClick={() =>
                     editor.isActive('subscript')
                         ? editor.chain().focus().unsetSubscript().run()
@@ -127,11 +144,11 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
                 className={cn(
                     getButtonClasses(editor),
                     editor.isActive('subscript') ? 'opacity-80' : '',
-                    'my-0.5',
                 )}>
                 <Subscript size={16} />
             </button>
             <button
+                title='Superscript'
                 onClick={() =>
                     editor.isActive('superscript')
                         ? editor.chain().focus().setSuperscript().run()
@@ -140,11 +157,11 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
                 className={cn(
                     getButtonClasses(editor),
                     editor.isActive('superscript') ? 'opacity-80' : '',
-                    'my-0.5',
                 )}>
                 <Superscript size={16} />
             </button>
             <button
+                title='Add link'
                 onClick={() =>
                     editor.isActive('link')
                         ? editor.chain().focus().unsetLink().run()
@@ -153,110 +170,98 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
                 className={cn(
                     getButtonClasses(editor),
                     editor.isActive('link') ? 'opacity-80' : '',
-                    'my-0.5',
                 )}>
                 <LinkIcon size={16} />
             </button>
             <button
+                title='Insert table'
                 onClick={() =>
-                    editor
-                        .chain()
-                        .focus()
-                        .insertContent(tableBoilerplate, {
-                            parseOptions: {
-                                preserveWhitespace: false,
-                            },
-                        })
-                        .run()
+                    editor.isActive('table')
+                        ? setShowTableMenu(!showTableMenu)
+                        : editor
+                              .chain()
+                              .focus()
+                              .insertContent(tableBoilerplate, {
+                                  parseOptions: {
+                                      preserveWhitespace: false,
+                                  },
+                              })
+                              .run()
                 }
                 className={getButtonClasses(editor)}>
                 <Table size={16} strokeWidth={1.8} />
             </button>
-            {editor.isActive('table') && (
-                <>
+            {editor.isActive('table') && showTableMenu && (
+                <motion.div
+                    className={cn(
+                        'absolute mt-2 px-1 py-0.5 z-10 bg-white rounded border shadow-inner',
+                    )}
+                    initial={{ opacity: 0, x: 0, y: -50 }}
+                    animate={{ opacity: 1, x: 0, y: 0 }}
+                    exit={{ opacity: 0, x: 0, y: -50 }}
+                    transition={{
+                        duration: 0.3,
+                        delay: 0,
+                        type: 'spring',
+                    }}>
                     <button
                         onClick={() =>
                             editor.chain().focus().addColumnBefore().run()
                         }
                         disabled={!editor.can().addColumnBefore()}
-                        className={cn(
-                            getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
-                        )}>
-                        Add column before
+                        className={cn(getButtonClasses(editor), tableClasses)}>
+                        <BetweenVerticalStart size={16} className='mr-1' /> Add
+                        column before
                     </button>
                     <button
                         onClick={() =>
                             editor.chain().focus().addColumnAfter().run()
                         }
                         disabled={!editor.can().addColumnAfter()}
-                        className={cn(
-                            getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
-                        )}>
-                        Add column after
+                        className={cn(getButtonClasses(editor), tableClasses)}>
+                        <BetweenVerticalEnd size={16} className='mr-1' /> Add
+                        column after
                     </button>
                     <button
                         onClick={() =>
                             editor.chain().focus().deleteColumn().run()
                         }
                         disabled={!editor.can().deleteColumn()}
-                        className={cn(
-                            getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
-                        )}>
-                        Delete column
+                        className={cn(getButtonClasses(editor), tableClasses)}>
+                        <Grid2x2X size={16} className='mr-1' /> Delete column
                     </button>
                     <button
                         onClick={() =>
                             editor.chain().focus().addRowBefore().run()
                         }
                         disabled={!editor.can().addRowBefore()}
-                        className={cn(
-                            getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
-                        )}>
-                        Add row before
+                        className={cn(getButtonClasses(editor), tableClasses)}>
+                        <BetweenHorizonalStart size={16} className='mr-1' /> Add
+                        row before
                     </button>
                     <button
                         onClick={() =>
                             editor.chain().focus().addRowAfter().run()
                         }
                         disabled={!editor.can().addRowAfter()}
-                        className={cn(
-                            getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
-                        )}>
-                        Add row after
+                        className={cn(getButtonClasses(editor), tableClasses)}>
+                        <BetweenHorizonalEnd size={16} className='mr-1' /> Add
+                        row after
                     </button>
                     <button
                         onClick={() => editor.chain().focus().deleteRow().run()}
                         disabled={!editor.can().deleteRow()}
-                        className={cn(
-                            getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
-                        )}>
-                        Delete row
+                        className={cn(getButtonClasses(editor), tableClasses)}>
+                        <Grid2x2X size={16} className='mr-1' /> Delete row
                     </button>
-                    <button
-                        onClick={() =>
-                            editor.chain().focus().deleteTable().run()
-                        }
-                        disabled={!editor.can().deleteTable()}
-                        className={cn(
-                            getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
-                        )}>
-                        Delete table
-                    </button>
-                    <button
+                    {/* <button
                         onClick={() =>
                             editor.chain().focus().mergeCells().run()
                         }
                         disabled={!editor.can().mergeCells()}
                         className={cn(
                             getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
+                            tableClasses,
                         )}>
                         Merge cells
                     </button>
@@ -265,18 +270,18 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
                         disabled={!editor.can().splitCell()}
                         className={cn(
                             getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
+                            tableClasses,
                         )}>
-                        Split cell
-                    </button>
-                    <button
+                        <SquareSplitHorizontal size={16} className="mr-1" /> Split cell
+                    </button> */}
+                    {/* <button
                         onClick={() =>
                             editor.chain().focus().toggleHeaderColumn().run()
                         }
                         disabled={!editor.can().toggleHeaderColumn()}
                         className={cn(
                             getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
+                            tableClasses,
                         )}>
                         Toggle Header Column
                     </button>
@@ -287,19 +292,17 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
                         disabled={!editor.can().toggleHeaderRow()}
                         className={cn(
                             getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
+                            tableClasses,
                         )}>
                         Toggle Header Row
-                    </button>
+                    </button> */}
                     <button
                         onClick={() =>
                             editor.chain().focus().toggleHeaderCell().run()
                         }
                         disabled={!editor.can().toggleHeaderCell()}
-                        className={cn(
-                            getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
-                        )}>
+                        className={cn(getButtonClasses(editor), tableClasses)}>
+                        <Heading size={16} className='mr-1' />
                         Toggle Header Cell
                     </button>
                     <button
@@ -307,13 +310,11 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
                             editor.chain().focus().mergeOrSplit().run()
                         }
                         disabled={!editor.can().mergeOrSplit()}
-                        className={cn(
-                            getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
-                        )}>
-                        Merge or split
+                        className={cn(getButtonClasses(editor), tableClasses)}>
+                        <TableColumnsSplit size={16} className='mr-1' /> Merge
+                        or split
                     </button>
-                    <button
+                    {/* <button
                         onClick={() =>
                             editor
                                 .chain()
@@ -328,27 +329,24 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
                         }
                         className={cn(
                             getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
+                            tableClasses,
                         )}>
                         Set cell attribute
-                    </button>
+                    </button> */}
                     <button
                         onClick={() => editor.chain().focus().fixTables().run()}
                         disabled={!editor.can().fixTables()}
-                        className={cn(
-                            getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
-                        )}>
-                        Fix tables
+                        className={cn(getButtonClasses(editor), tableClasses)}>
+                        <WandSparkles size={16} className='mr-1' /> Fix tables
                     </button>
-                    <button
+                    {/* <button
                         onClick={() =>
                             editor.chain().focus().goToNextCell().run()
                         }
                         disabled={!editor.can().goToNextCell()}
                         className={cn(
                             getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
+                            tableClasses,
                         )}>
                         Go to next cell
                     </button>
@@ -359,11 +357,23 @@ export default function CompleteBubbleMenu({ editor }: { editor: Editor }) {
                         disabled={!editor.can().goToPreviousCell()}
                         className={cn(
                             getButtonClasses(editor),
-                            'h-6 border-none w-72 text-left text-sm',
+                            tableClasses,
                         )}>
                         Go to previous cell
+                    </button> */}
+                    <button
+                        onClick={() =>
+                            editor.chain().focus().deleteTable().run()
+                        }
+                        disabled={!editor.can().deleteTable()}
+                        className={cn(
+                            getButtonClasses(editor),
+                            tableClasses,
+                            'text-red-600',
+                        )}>
+                        <Trash size={16} className='mr-1' /> Delete table
                     </button>
-                </>
+                </motion.div>
             )}
         </BubbleMenu>
     );
--- src/components/ui/file/NoteView/HighlightDropdown.tsx
+++ src/components/ui/file/NoteView/HighlightDropdown.tsx
@@ -3,62 +3,104 @@
 import * as React from 'react';
 import { Editor } from '@tiptap/core';
 import { cn } from '~/hooks/utils';
+import { Circle, CircleSlash } from 'lucide-react';
+import getButtonClasses from './getButtonClasses';
+import { motion } from 'framer-motion';
 
 // TODO
 
-const colors: string[] = [
-    '#ffc078',
-    '#8ce99a',
-    '#74c0fc',
-    '#b197fc',
-];
+const colors: string[] = ['#ffc078', '#8ce99a', '#74c0fc', '#b197fc'];
 
 function getCurrentColor(editor: Editor) {
-    colors.forEach(color => {
-        if(editor.isActive('highlight', { color: color })) return color;
+    colors.forEach((color) => {
+        if (editor.isActive('highlight', { color: color })) return color;
     });
     return false;
 }
 
-function Color({ className }: { className?: string }) {
-    return <div className={cn("w-4 h-4 mx-1 border rounded-full shadow-inner", className)}></div>
+function Color({ color, className }: { color: string; className?: string }) {
+    return <Circle size={16} fill={color} className={className} />;
 }
 
 function NoColor({ className }: { className?: string }) {
-    return '/'
+    return (
+        <CircleSlash
+            size={16}
+            fill='#49477f'
+            fillOpacity={0.4}
+            className={className}
+        />
+    );
 }
 
-export function HighlightDropdown({ editor }: { editor: Editor }) {
+export function HighlightDropdown({
+    editor,
+    requirements,
+}: {
+    editor: Editor;
+    requirements?: {};
+}) {
     const [dropdownState, setDropdownState] = React.useState(false);
+    React.useEffect(() => {
+        setDropdownState(false);
+    }, [requirements]);
     return (
         <>
             <button
-                onClick={() => setDropdownState(true)}
-                className={editor.isActive('highlight') ? 'is-active' : ''}>
-                {getCurrentColor(editor) ? <Color className={`bg-${getCurrentColor(editor)}`} /> : <NoColor />}
+                onClick={() => setDropdownState(!dropdownState)}
+                className={cn('z-30', getButtonClasses(editor, 'highlight'))}>
+                {getCurrentColor(editor) ? (
+                    <Color color={String(getCurrentColor(editor))} />
+                ) : (
+                    <NoColor />
+                )}
             </button>
-            {dropdownState && <div className={cn("absolute")}><>{colors.map((color) => {
-                <button
-                    onClick={() =>
-                        editor
-                            .chain()
-                            .focus()
-                            .toggleHighlight({ color: color })
-                            .run()
-                    }
-                    className={
-                        editor.isActive('highlight', { color: color })
-                            ? 'is-active'
-                            : ''
-                    }>
-                    <Color className={`bg-${color}`} />
-                </button>
-            })}</>
-            <button
-                onClick={() => editor.chain().focus().unsetHighlight().run()}
-                disabled={!editor.isActive('highlight')}>
-                <NoColor />
-            </button></div> }
+            {dropdownState && (
+                <motion.div
+                    className={cn(
+                        'absolute h-6 px-1 py-0.5 z-20 bg-white rounded-b border-x border-b shadow-inner -translate-y-1',
+                    )}
+                    style={{ left: '100px' }}
+                    initial={{ opacity: 0, x: 0, y: -50 }}
+                    animate={{ opacity: 1, x: 0, y: 0 }}
+                    transition={{
+                        duration: 0.3,
+                        delay: 0,
+                        type: 'spring',
+                    }}>
+                    <>
+                        {colors.map((color) => (
+                            <button
+                                title='Highlight'
+                                key={color}
+                                onClick={() =>
+                                    editor
+                                        .chain()
+                                        .focus()
+                                        .toggleHighlight({ color: color })
+                                        .run()
+                                }
+                                className={
+                                    editor.isActive('highlight', {
+                                        color: color,
+                                    })
+                                        ? 'is-active'
+                                        : ''
+                                }>
+                                <Color color={color} className='mr-1' />
+                            </button>
+                        ))}
+                    </>
+                    <button
+                        title='Remove highlight'
+                        onClick={() =>
+                            editor.chain().focus().unsetHighlight().run()
+                        }
+                        disabled={!editor.isActive('highlight')}>
+                        <NoColor />
+                    </button>
+                </motion.div>
+            )}
         </>
     );
 }
